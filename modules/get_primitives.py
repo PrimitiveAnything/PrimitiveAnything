8cbc38804b6815edf5b2a9db55121e265ff32b00
avyasharma <avya14.sharma@gmail.com>
2025-11-29 12:10:34 -0500

    Updated to make logvar

--- modules/get_primitives.py
+++ modules/get_primitives.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 from primitives import CuboidSurface, EllipsoidSurface, EllipticalCylinderSurface
-from .sample import get_sample, get_sample_and_probs
+from .sample import get_sample_and_probs
 
 def get_samples(embedding):
     """
@@ -9,23 +9,24 @@ def get_samples(embedding):
     a B x 1 x 11 tensor (scale, rotation, translation, class)
     """
     B, nPart, _ = embedding.shape
-    scale_mean, scale_std = embedding[:, :, 0:3], embedding[:, :, 3:6]
-    rot_mean, rot_std = embedding[:, :, 6:10], embedding[:, :, 10:14]
-    trans_mean, trans_std = embedding[:, :, 14:17], embedding[:, :, 17:20]
+    scale_mean, scale_log_var = embedding[:, :, 0:3], embedding[:, :, 3:6]
+    rot_mean, rot_log_var = embedding[:, :, 6:10], embedding[:, :, 10:14]
+    trans_mean, trans_log_var = embedding[:, :, 14:17], embedding[:, :, 17:20]
     probs = F.softmax(embedding[:, :, 20:], dim=-1)
 
     next_state = torch.empty((B, nPart, 11), dtype=torch.float64, device=embedding.device)
 
-    scale = get_sample(scale_mean, scale_std)
-    rotation = get_sample(rot_mean, rot_std)
-    translation = get_sample(trans_mean, trans_std)
+    
+    scale, scale_probs = get_sample_and_probs(scale_mean, scale_log_var)
+    quaternion, quaternion_probs = get_sample_and_probs(rot_mean, rot_log_var)
+    translation, translation_probs = get_sample_and_probs(trans_mean, trans_log_var)
 
     next_state[:, :, 0:3] = scale 
-    next_state[:, :, 3:7] = rotation 
+    next_state[:, :, 3:7] = quaternion 
     next_state[:, :, 7:10] = translation 
     next_state[:, :, 10] = torch.argmax(probs, dim=-1)
 
-    return next_state
+    return next_state, scale_probs + quaternion_probs + translation_probs
 
 def get_primitives(samples):
     """
--- modules/sample.py
+++ modules/sample.py
@@ -1,24 +1,19 @@
 import torch
 
-def get_probs(mean, std_dev, sample):
-    var = std_dev ** 2
-    coeff = 1.0 /(torch.sqrt(2 * torch.pi * var))
+def get_log_probs(mean, logvar, sample):
+    return -0.5 * (
+        torch.log(2 * torch.pi) +
+        logvar +
+        (sample - mean)**2 * torch.exp(-logvar)
+    )
 
-    exponent = -0.5 * ((sample - mean) ** 2) / var
-    return coeff * torch.exp(exponent)
-
-def get_log_probs(mean, std_dev, sample):
-    var = std_dev ** 2
-    return -0.5 * (torch.log(2 * torch.pi * var) +
-                   ((sample - mean) ** 2) / var)
-
-def get_sample(mean, std_dev):
+def get_sample(mean, var):
+    std_dev = torch.sqrt(var)
     eps = torch.randn_like(mean)
     sample = mean + eps * std_dev
     return sample
 
-def get_sample_and_probs(mean, std_dev):
-    sample = get_sample
-    probs = get_probs(mean, std_dev, sample)
-    log_probs = get_log_probs(mean, std_dev, sample)
-    return sample, probs, log_probs
+def get_sample_and_probs(mean, logvar):
+    sample = get_sample(mean, torch.exp(logvar))
+    log_probs = get_log_probs(mean, logvar, sample)
+    return sample, log_probs
